d48c03bab18571be6a8f3a6900bbb3c8fb5664b9
Dmitry <berlindmytro@gmail.com>
2022-04-30 12:38:30 +0300

    add coments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
-  validates :description, presence: true, length: { minimum: 5 }
+  validates :description, presence: true
+  validates :body, presence: true, length: { minimum: 5 }
 end
