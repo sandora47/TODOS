e34ac84cb890cd03f241273cf78ab151d0701d31
Dmitry <berlindmytro@gmail.com>
2022-05-10 17:11:06 +0300

    comments to todos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  has_many :todos, dependent: :destroy
   has_secure_password
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :password, presence: true, length: { minimum: 4 }, confirmation: true
