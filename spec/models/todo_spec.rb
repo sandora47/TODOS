4c9ff0226000556bd4701f31ab8586f928fdd71b
sandora47 <testsingl8@gmail.com>
2022-05-14 22:32:38 +0300

    todo spec

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'before publication' do
+    it 'cannot have comments' do
+      expect { todo.save! }.not_to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  describe "Update" do
+    context "update comment with valid params" do
+      it "should not raise exception" do
+        expect{ todo.update!(title: 'valid') }.not_to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+    context "update comment with invalid params" do
+      it "should not raise exception" do
+        expect{ todo.update!(title: '') }.to raise_error
+      end
+    end
+    context "update user with valid params" do
+      it "should not raise exception" do
+        expect{ comment.update!(username: 'vali') }.to raise_error
+      end
+    end
+  end
 end
