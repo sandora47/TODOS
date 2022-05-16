ed992e5b4169a81445db11a54d9bbae2ffcd3ac1
sandora47 <testsingl8@gmail.com>
2022-05-16 14:55:22 +0300

    images
    add images to footer
    
    @
    
    fixed
    
    upd
    
    footer background
    
    bacground images

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,8 @@
 class Todo < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
+
+  def formatted_created_at
+    created_at.strftime('%Y-%m-%d %H:%M:%S')
+  end
 end
